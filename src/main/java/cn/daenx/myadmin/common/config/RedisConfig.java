10ad9458084036586d205480ec60d76b4d1178ee
Daen <1330166565@qq.com>
2023-02-08 09:38:07 +0800

    集成redis和工具类

---
+++ src/main/java/cn/daenx/myadmin/common/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package cn.daenx.myadmin.common.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+/**
+ * Redis配置类
+ * @author DaenMax
+ */
+@Configuration
+public class RedisConfig {
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 解决redis插入中文乱码
+     * @return
+     */
+    @Bean
+    public RedisTemplate redisTemplateInit() {
+        //设置序列化Key的实例化对象
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置序列化Value的实例化对象
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
--- src/main/java/cn/daenx/myadmin/test/controller/TestController.java
+++ src/main/java/cn/daenx/myadmin/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package cn.daenx.myadmin.test.controller;
 
+import cn.daenx.myadmin.common.utils.RedisUtil;
 import cn.daenx.myadmin.test.po.TbUser;
 import cn.daenx.myadmin.test.po.TestName;
 import cn.daenx.myadmin.test.service.TbUserService;
@@ -13,27 +14,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@Tag(name = "姓名管理",description = "对系统内的姓名进行增删改查")
+
+@Tag(name = "功能测试", description = "对系统内的一些功能进行测试和展示使用方法")
 @RestController
-@RequestMapping("/name")
+@RequestMapping("/test")
 public class TestController {
     @Resource
     private TestNameService testNameService;
     @Resource
     private TbUserService tbUserService;
+    @Resource
+    private RedisUtil redisUtil;
 
-    @Operation(method = "肯定是GET啦",summary = "获取数据源一的数据",description = "我是description")
+    /**
+     * 获取数据源一的数据
+     *
+     * @return
+     */
+    @Operation(method = "肯定是GET啦", summary = "获取数据源一的数据", description = "我是description")
     @Parameter
     @GetMapping("/list1")
-    public String list1(){
+    public String list1() {
         List<TestName> list = testNameService.list();
         System.out.println(list);
         return "aaa";
     }
+
+    /**
+     * 获取数据源二的数据
+     *
+     * @return
+     */
     @GetMapping("/list2")
-    public String list2(){
+    public String list2() {
         List<TbUser> list = tbUserService.list();
         System.out.println(list);
         return "bbb";
+    }
+
+    /**
+     * 测试redis
+     *
+     * @return
+     */
+    @GetMapping("/test")
+    public String test() {
+        redisUtil.del("test");
+        redisUtil.set("test", "你好啊");
+        String test = (String) redisUtil.get("test");
+        System.out.println(test);
+        return "test";
     }
 }
