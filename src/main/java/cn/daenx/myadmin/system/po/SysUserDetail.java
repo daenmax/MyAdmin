d84045c6955a6b81638f49415c5666d3f81392ce
Daen <1330166565@qq.com>
2023-02-15 16:27:19 +0800

    优化

--- src/main/java/cn/daenx/myadmin/system/po/SysUserDetail.java
+++ src/main/java/cn/daenx/myadmin/system/po/SysUserDetail.java
@@ -5,15 +5,20 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
 import java.io.Serializable;
 import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+/**
+ * 用户信息表
+ */
 @Data
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName(value = "sys_user_detail")
