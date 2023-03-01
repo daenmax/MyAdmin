c9e77842e339d9ef04d47eeec29736b3ab636554
Daen <1330166565@qq.com>
2023-03-01 18:22:44 +0800

    调整日志输出和写出格式

--- src/main/java/cn/daenx/myadmin/MyAdminApplication.java
+++ src/main/java/cn/daenx/myadmin/MyAdminApplication.java
@@ -1,5 +1,6 @@
 package cn.daenx.myadmin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,10 +9,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@Slf4j
 public class MyAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyAdminApplication.class, args);
+        log.info("我是info");
+        log.warn("我是warn");
+        log.error("我是error");
     }
 
 }
