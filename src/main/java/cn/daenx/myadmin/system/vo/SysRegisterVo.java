18009b1678325dd7606a7501a43a77f63816696c
Daen <1330166565@qq.com>
2023-03-22 11:11:27 +0800

    完善注册、修改密码时的长度控制

--- src/main/java/cn/daenx/myadmin/system/vo/SysRegisterVo.java
+++ src/main/java/cn/daenx/myadmin/system/vo/SysRegisterVo.java
@@ -1,6 +1,8 @@
 package cn.daenx.myadmin.system.vo;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class SysRegisterVo{
@@ -13,6 +15,10 @@ public class SysRegisterVo{
      * 验证码ID
      */
     private String uuid;
+    @NotBlank(message = "账号不能为空")
+    @Length(min = 5,max = 30,message = "账号长度需要在5到30个字符")
     private String username;
+    @NotBlank(message = "密码不能为空")
+    @Length(min = 6,max = 16,message = "密码长度需要在6到16个字符")
     private String password;
 }
