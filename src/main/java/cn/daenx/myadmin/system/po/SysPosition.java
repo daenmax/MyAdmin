313d201246b06b33f8ecb1523a1d90f87b5b5b0e
DaenMax <1330166565@qq.com>
2023-04-13 23:32:33 +0800

    岗位导出增加了响应注解

--- src/main/java/cn/daenx/myadmin/system/po/SysPosition.java
+++ src/main/java/cn/daenx/myadmin/system/po/SysPosition.java
@@ -1,6 +1,10 @@
 package cn.daenx.myadmin.system.po;
 
+import cn.daenx.myadmin.common.annotation.Dict;
+import cn.daenx.myadmin.common.excel.DictConverter;
 import cn.daenx.myadmin.common.vo.BaseEntity;
+import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -22,6 +26,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName(value = "sys_position")
+//导出时忽略没有@ExcelProperty的字段
+@ExcelIgnoreUnannotated
 public class SysPosition extends BaseEntity implements Serializable {
     @TableId(value = "id", type = IdType.ASSIGN_UUID)
     private String id;
@@ -29,36 +35,44 @@ public class SysPosition extends BaseEntity implements Serializable {
     /**
      * 岗位名称
      */
+    @ExcelProperty(value = "岗位名称")
     @TableField(value = "`name`")
     private String name;
 
     /**
      * 岗位编码
      */
+    @ExcelProperty(value = "岗位编码")
     @TableField(value = "code")
     private String code;
 
     /**
      * 岗位简介
      */
+    @ExcelProperty(value = "岗位简介")
     @TableField(value = "summary")
     private String summary;
 
     /**
      * 排序
      */
+    @ExcelProperty(value = "排序")
     @TableField(value = "sort")
     private Integer sort;
 
     /**
      * 岗位状态，0=正常，1=禁用
      */
+    @ExcelProperty(value = "字典状态", converter = DictConverter.class)
+//    @Dict(custom = {@DictDetail(value = "0", label = "正常"), @DictDetail(value = "1", label = "禁用")})
+    @Dict(dictCode = "sys_normal_disable", custom = {})
     @TableField(value = "`status`")
     private String status;
 
     /**
      * 备注
      */
+    @ExcelProperty(value = "备注")
     @TableField(value = "remark")
     private String remark;
 }
