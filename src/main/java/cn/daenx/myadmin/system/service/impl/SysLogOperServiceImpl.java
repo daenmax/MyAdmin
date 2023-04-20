222ab34803145d93d5a85c98af2d6d92fc85366b
Daen <1330166565@qq.com>
2023-04-20 10:34:10 +0800

    记录日志改为异步

--- src/main/java/cn/daenx/myadmin/system/service/impl/SysLogOperServiceImpl.java
+++ src/main/java/cn/daenx/myadmin/system/service/impl/SysLogOperServiceImpl.java
@@ -3,6 +3,7 @@ package cn.daenx.myadmin.system.service.impl;
 import cn.daenx.myadmin.common.utils.MyUtil;
 import cn.hutool.core.util.ObjectUtil;
 import jakarta.annotation.Resource;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import cn.daenx.myadmin.system.mapper.SysLogOperMapper;
@@ -19,6 +20,7 @@ public class SysLogOperServiceImpl extends ServiceImpl<SysLogOperMapper, SysLogO
      *
      * @param sysLogOper
      */
+    @Async
     @Override
     public void saveOper(SysLogOper sysLogOper) {
         if (ObjectUtil.isNotEmpty(sysLogOper.getRequestIp())) {
