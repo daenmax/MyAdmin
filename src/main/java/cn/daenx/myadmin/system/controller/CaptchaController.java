26c5aaad3fd14654616e3a74f4c8eec94d1f1005
DaenMax <1330166565@qq.com>
2023-05-13 13:44:58 +0800

    完成短信测试

--- src/main/java/cn/daenx/myadmin/system/controller/CaptchaController.java
+++ src/main/java/cn/daenx/myadmin/system/controller/CaptchaController.java
@@ -1,8 +1,10 @@
 package cn.daenx.myadmin.system.controller;
 
+import cn.daenx.myadmin.common.utils.SmsUtil;
 import cn.daenx.myadmin.common.vo.Result;
 import cn.daenx.myadmin.system.service.CaptchaService;
 import cn.daenx.myadmin.system.service.SysConfigService;
+import cn.daenx.myadmin.system.vo.system.SmsSendResult;
 import cn.dev33.satoken.annotation.SaIgnore;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @SaIgnore
 @RestController
@@ -41,6 +44,24 @@ public class CaptchaController {
 //        SysEmailConfigVo.Email email = EmailUtil.getOneEmailConfig();
 //        System.out.println(email.getEmail());
         return Result.ok("");
+    }
+    /**
+     * 测试邮箱机制
+     *
+     * @return
+     */
+    @GetMapping("/testSms")
+    public Result testSms() {
+        //腾讯云
+//        Map<String, String> map1 = new HashMap<>();
+//        map1.put("1", "1234");
+//        map1.put("2", "8888");
+//        SmsSendResult smsSendResult = SmsUtil.sendSms("+8618731055555,+8618754158888", "1794869", map1,"tencent");
+        //阿里云
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("code", "6666");
+        SmsSendResult smsSendResult = SmsUtil.sendSms("+8618731055555,+8618754158888", "SMS_460755481", map2,"aliyun");
+        return Result.ok(smsSendResult);
     }
 
 }
