ed12e2cda96261362df51afb16c2555a11c9d86b
DaenMax <1330166565@qq.com>
2023-05-13 12:11:10 +0800

    优化短信工具类参数说明

--- src/main/java/cn/daenx/myadmin/common/utils/SmsUtil.java
+++ src/main/java/cn/daenx/myadmin/common/utils/SmsUtil.java
@@ -37,12 +37,12 @@ public class SmsUtil {
      * 发送短信
      * <p>
      * <h4>关于phones参数</h4><br>
-     * 腾讯云：格式为+[国家或地区码][手机号]，单次请求最多支持200个手机号且要求全为境内手机号或全为境外手机号<br><br>
+     * 腾讯云：格式为+[国家或地区码][手机号]，单次请求最多支持200个手机号且要求全为境内手机号或全为境外手机号，发送国内短信格式还支持0086、86或无任何国家或地区码的11位手机号码，前缀默认为+86。<br><br>
      * 阿里云：国内短信：+/+86/0086/86或无任何前缀的11位手机号码，例如1390000****。国际/港澳台消息：国际区号+号码，例如852000012****。单次上限为1000个手机号码
      * </p>
      * <p>
      * <h4>关于param参数</h4><br>
-     * 腾讯云：例如模板为：验证码为：{1}，您正在进行{2}，如非本人操作，请忽略本短信。那么key=1，value为1234，key=2，value=找回密码<br><br>
+     * 腾讯云：例如模板为：验证码为：{1}，如非本人操作，请忽略本短信。那么key=1，value为1234<br><br>
      * 阿里云：例如模板为：您的验证码为：${code}，请勿泄露于他人！那么key=code，value为1234
      * </p>
      *
@@ -74,12 +74,12 @@ public class SmsUtil {
      * 发送短信
      * <p>
      * <h4>关于phones参数</h4><br>
-     * 腾讯云：格式为+[国家或地区码][手机号]，单次请求最多支持200个手机号且要求全为境内手机号或全为境外手机号<br><br>
+     * 腾讯云：格式为+[国家或地区码][手机号]，单次请求最多支持200个手机号且要求全为境内手机号或全为境外手机号，发送国内短信格式还支持0086、86或无任何国家或地区码的11位手机号码，前缀默认为+86。<br><br>
      * 阿里云：国内短信：+/+86/0086/86或无任何前缀的11位手机号码，例如1390000****。国际/港澳台消息：国际区号+号码，例如852000012****。单次上限为1000个手机号码
      * </p>
      * <p>
      * <h4>关于param参数</h4><br>
-     * 腾讯云：例如模板为：验证码为：{1}，您正在进行{2}，如非本人操作，请忽略本短信。那么key=1，value为1234，key=2，value=找回密码<br><br>
+     * 腾讯云：例如模板为：验证码为：{1}，如非本人操作，请忽略本短信。那么key=1，value为1234<br><br>
      * 阿里云：例如模板为：您的验证码为：${code}，请勿泄露于他人！那么key=code，value为1234
      * </p>
      *
@@ -115,7 +115,7 @@ public class SmsUtil {
      * 发送短信协议，阿里云
      *
      * @param smsInfo
-     * @param phones     多个手机号用,隔开，需要加+86等前缀
+     * @param phones     多个手机号用,隔开
      * @param templateId
      * @param param      例如模板为：您的验证码为：${code}，请勿泄露于他人！那么key=code，value为1234
      * @return
@@ -133,6 +133,8 @@ public class SmsUtil {
                     .setTemplateCode(templateId)
                     .setTemplateParam(JSON.toJSONString(param));
             SendSmsResponse resp = client.sendSms(req);
+            System.out.println("OK".equals(resp.getBody().getCode()));
+            System.out.println(resp.getBody().getCode());
             SmsSendResult smsSendResult = SmsSendResult.builder().isSuccess("OK".equals(resp.getBody().getCode())).msg(resp.getBody().getMessage()).aliyunRes(resp).build();
             return smsSendResult;
         } catch (Exception e) {
@@ -148,7 +150,7 @@ public class SmsUtil {
      * @param smsInfo
      * @param phones     多个手机号用,隔开，需要加+86等前缀
      * @param templateId
-     * @param param      例如模板为：验证码为：{1}，您正在进行{2}，如非本人操作，请忽略本短信。那么key=1，value为1234，key=2，value=找回密码
+     * @param param      例如模板为：验证码为：{1}，如非本人操作，请忽略本短信。那么key=1，value为1234
      * @return
      */
     private static SmsSendResult sendSmsTencent(SysSmsConfigVo.SmsInfo smsInfo, String phones, String templateId, Map<String, String> param) {
