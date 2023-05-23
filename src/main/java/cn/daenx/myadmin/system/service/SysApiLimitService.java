0c7f490fabe2666a6c702b6353b76e7a00e54080
DaenMax <1330166565@qq.com>
2023-05-23 09:31:13 +0800

    增加接口限制表

---
+++ src/main/java/cn/daenx/myadmin/system/service/SysApiLimitService.java
@@ -0,0 +1,8 @@
+package cn.daenx.myadmin.system.service;
+
+import cn.daenx.myadmin.system.po.SysApiLimit;
+import com.baomidou.mybatisplus.extension.service.IService;
+public interface SysApiLimitService extends IService<SysApiLimit>{
+
+
+}
---
+++ src/main/java/cn/daenx/myadmin/system/service/impl/SysApiLimitServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.daenx.myadmin.system.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import cn.daenx.myadmin.system.mapper.SysApiLimitMapper;
+import cn.daenx.myadmin.system.po.SysApiLimit;
+import cn.daenx.myadmin.system.service.SysApiLimitService;
+@Service
+public class SysApiLimitServiceImpl extends ServiceImpl<SysApiLimitMapper, SysApiLimit> implements SysApiLimitService{
+
+}
