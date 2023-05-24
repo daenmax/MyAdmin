ed0e81e97b5db8d2be278468fac3e77befb649c2
DaenMax <1330166565@qq.com>
2023-05-24 18:03:33 +0800

    优化

--- src/main/java/cn/daenx/myadmin/test/controller/TestController.java
+++ src/main/java/cn/daenx/myadmin/test/controller/TestController.java
@@ -1,20 +1,12 @@
 package cn.daenx.myadmin.test.controller;
 
-import cn.daenx.myadmin.common.oss.core.OssClient;
-import cn.daenx.myadmin.common.oss.utils.OssUtil;
-import cn.daenx.myadmin.common.oss.vo.UploadResult;
-import cn.daenx.myadmin.common.utils.RedisUtil;
+
 import cn.daenx.myadmin.common.vo.Result;
 import cn.dev33.satoken.annotation.SaIgnore;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
 
 
 @Tag(name = "功能测试", description = "对系统内的一些功能进行测试和展示使用方法")
@@ -25,10 +17,11 @@ import java.util.List;
 public class TestController {
 
     /**
-     * 测试
+     * 测试接口
      *
      * @return
      */
+    @Operation(method = "肯定是GET啦", summary = "测试接口", description = "我是description")
     @GetMapping("/test")
     public Result test() {
         return Result.ok("查询成功");
