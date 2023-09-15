a94d18c5f23d05656d8ea53dcb938694b346e35d
DaenMax <1330166565@qq.com>
2023-09-15 09:55:04 +0800

    1.优化了druid统计SQL和监控页面配置错误问题 2.去掉了druid监控页面的banner广告

--- src/main/java/cn/daenx/framework/common/filter/RepeatableFilter.java
+++ src/main/java/cn/daenx/framework/common/filter/RepeatableFilter.java
@@ -22,8 +22,7 @@ public class RepeatableFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         ServletRequest requestWrapper = null;
-        if (request instanceof HttpServletRequest
-                && StringUtils.startsWithIgnoreCase(request.getContentType(), MediaType.APPLICATION_JSON_VALUE)) {
+        if (request instanceof HttpServletRequest && StringUtils.startsWithIgnoreCase(request.getContentType(), MediaType.APPLICATION_JSON_VALUE)) {
             requestWrapper = new RepeatedlyRequestWrapper((HttpServletRequest) request, response);
         }
         if (null == requestWrapper) {
