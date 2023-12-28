fdc5d951347614b0d86cb4af6488547aa292ab4a
DaenMax <1330166565@qq.com>
2023-12-28 17:33:31 +0800

    EasyExcel导入导出工具类增加了导出多个sheet的方法

--- src/main/java/cn/daenx/framework/excel/utils/ExcelUtil.java
+++ src/main/java/cn/daenx/framework/excel/utils/ExcelUtil.java
@@ -5,8 +5,10 @@ import cn.daenx.framework.excel.DefaultExcelListener;
 import cn.daenx.framework.excel.ExcelListener;
 import cn.daenx.framework.excel.ExcelResult;
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.write.builder.ExcelWriterBuilder;
 import com.alibaba.excel.write.builder.ExcelWriterSheetBuilder;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,6 +19,11 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
 
+/**
+ * EasyExcel导入导出工具类
+ *
+ * @author DaenMax
+ */
 @Slf4j
 public class ExcelUtil {
     /**
@@ -58,7 +65,7 @@ public class ExcelUtil {
     }
 
     /**
-     * 导出XLSX
+     * 导出XLSX（只有一个sheet）
      *
      * @param response
      * @param fileName    导出的文件名，不需要加.xlsx
@@ -83,6 +90,54 @@ public class ExcelUtil {
         ExcelWriterBuilder write = EasyExcel.write(outputStream, entityClass);
         ExcelWriterSheetBuilder sheet = write.sheet(sheetName);
         sheet.doWrite(list);
+    }
+
+    /**
+     * 创建xlsx导出开始（适用于多个sheet）
+     * 使用案例：
+     * ExcelWriter writer = ExcelUtil.createExport(response, "测试文件");
+     * ExcelUtil.writeSheet(writer, "合同列表", list, ContractPageDto.class);
+     * ExcelUtil.writeSheet(writer, "流水列表", listBills, BusinessBillsPageDto.class);
+     * ExcelUtil.finishWrite(writer);
+     *
+     * @param response
+     * @param fileName 导出的文件名，不需要加.xlsx
+     */
+    public static <T> ExcelWriter createExport(HttpServletResponse response, String fileName) {
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        try {
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8") + ".xlsx");
+        } catch (UnsupportedEncodingException e) {
+            throw new MyException("导出XLS时发送错误");
+        }
+        OutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+        } catch (IOException e) {
+            throw new MyException("导出XLS发送错误");
+        }
+        ExcelWriter excelWriter = EasyExcel.write(outputStream).build();
+        return excelWriter;
+    }
+
+    /**
+     * 写入一个sheet
+     *
+     * @param sheetName   工作表名
+     * @param list<T>
+     * @param entityClass
+     */
+    public static <T> void writeSheet(ExcelWriter excelWriter, String sheetName, List<T> list, Class entityClass) {
+        WriteSheet sheet = EasyExcel.writerSheet(sheetName).head(entityClass).build();
+        excelWriter.write(list, sheet);
+    }
+
+    /**
+     * 结束写入sheet并导出
+     */
+    public static <T> void finishWrite(ExcelWriter excelWriter) {
+        excelWriter.finish();
     }
 
 }
